3b0f3b3d33b3790d84febf1d657aa660af8cdcf6
DavidChoi76 <choiyd1115@gmail.comn>
2023-08-27 14:45:29 +0900

    add requirements.txt and revise codes

--- my5k_map_2023.py
+++ my5k_map_2023.py
@@ -11,32 +11,16 @@ st.set_page_config(
     layout="wide",
 )
 
-"# 함께걸어요 My5K 진행 현황"
-"""streamlit-folium integrates two great open-source projects in the Python ecosystem:
-[Streamlit](https://streamlit.io) and
-[Folium](https://python-visualization.github.io/folium/)!"""
+"# 2023 함께걸어요 My5K 진행 현황"
 
 m = folium.Map(location=[39.949610, 175.150282], tiles="OpenStreetMap", zoom_start=2.4)  # 39.949610, 175.150282
 
 icons = folium.features.CustomIcon('5k_flag.png', icon_size=(20,20))
-#folium.Marker(
-#            [-6.353875, 36.141439], 
-#            popup="탄자니아\n다르에스살람\n100명", 
-#            icon=icon, 
-#            tooltip="탄자니아\n다르에스살람\n100명"
-#        ).add_to(m)
 
 # add marker one by one on the map
 data = pd.read_csv("data.csv", encoding='CP949')
 
-#m.add_circle_markers_from_xy(data, x="lon", y="lat", popup=icon) # add the image column to the popup parameter
 # add marker one by one on the map
-#for i in range(0, len(data)):
-#   folium.Marker(
-#      location=[data.iloc[i]['lat'], data.iloc[i]['lon']],
-#      popup=data.iloc[i]['name'],
-#   ).add_to(m)
-
 
 for i in range(0, len(data)):
    folium.Marker(
